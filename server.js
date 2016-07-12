38c0929ed971a98f1eed5aa066c6cc79493fda56
SmileAwhile <cjdunn11@gmail.com>
2016-07-12 14:37:45 -0700

    ready to go live

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-var express = require('express')
-var app = express();
+var express = require('express');
+var multer = require('multer'),
+bodyParser = require('body-parser'),
+path = require('path');
+var app = new express();
+app.use(bodyParser.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res){ res.render('index'); });  // show jade file for uploading
 
-    res.end('hello');
+app.post('/', multer({ dest: './uploads/'}).single('upl'),  // post to /uploads
+function(req,res){ console.log(req.body); //form fields
 
-});
+res.end(JSON.stringify({ size: req.file.size })); // display file size 
 
-var port = process.env.PORT || 3000;
-app.listen(port);
+res.status(204).end(); });
+
+var port = 3000;
+app.listen( port, function(){
+  console.log('listening on port '+port);
+} );
